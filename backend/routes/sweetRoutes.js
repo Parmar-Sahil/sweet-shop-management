211890cc571f21557dc62ad78e5ef0f07fb6d776
Sahil Parmar <sahilparmar7033030@gmail.com>
2025-07-16 12:56:51 +0530

    Add POST /sweets/:id/restock route connected to controller

--- backend/routes/sweetRoutes.js
+++ backend/routes/sweetRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import { addSweet,deleteSweet,getAllSweets,searchAndSort,purchaseSweet } from '../controllers/sweetController.js';
+import { addSweet,deleteSweet,getAllSweets,searchAndSort,purchaseSweet,restockSweet } from '../controllers/sweetController.js';
 
 const router = express.Router();
 router.post('/', addSweet);
 
 router.delete('/:id', deleteSweet);
 router.get('/', getAllSweets);
-
 router.get('/search', searchAndSort);
 router.post('/:id/purchase', purchaseSweet);
+router.post('/:id/restock', restockSweet);
 
 
 export default router;

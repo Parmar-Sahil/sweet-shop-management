102dd3fad4abdbd996e85b14f7f5b65353f491c4
Sahil Parmar <sahilparmar7033030@gmail.com>
2025-07-16 12:34:18 +0530

    Add deleteSweet function to in-memory sweet model

--- backend/models/sweetModel.js
+++ backend/models/sweetModel.js
@@ -4,3 +4,11 @@ export const getAll = () => sweets;
 export const add = (sweet) => sweets.push(sweet);
 export const find = (id) => sweets.find(s => s.id === id);
 export const set = (list) => { sweets = list; };
+
+
+export const deleteSweet = (id) => {
+  const index = sweets.findIndex(s => s.id === id);
+  if (index !== -1) {
+    sweets.splice(index, 1);
+  }
+};

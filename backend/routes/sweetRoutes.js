615e08d3d55bc19fdbfb9ba9d93e1c7a4561bfbf
Sahil Parmar <sahilparmar7033030@gmail.com>
2025-07-16 12:46:41 +0530

    Add GET /sweets route connected to getAllSweets controller

--- backend/routes/sweetRoutes.js
+++ backend/routes/sweetRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addSweet,deleteSweet } from '../controllers/sweetController.js';
+import { addSweet,deleteSweet,getAllSweets } from '../controllers/sweetController.js';
 
 const router = express.Router();
 router.post('/', addSweet);
 
 router.delete('/:id', deleteSweet);
+router.get('/', getAllSweets);
 
 
 export default router;

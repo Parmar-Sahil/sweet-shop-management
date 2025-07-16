cada2e9e2cb1e297cf6c2593f1920f3d898d9149
Sahil Parmar <sahilparmar7033030@gmail.com>
2025-07-16 12:50:25 +0530

    Add GET /sweets/search route to support filtering and sorting

--- backend/routes/sweetRoutes.js
+++ backend/routes/sweetRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { addSweet,deleteSweet,getAllSweets } from '../controllers/sweetController.js';
+import { addSweet,deleteSweet,getAllSweets,searchAndSort } from '../controllers/sweetController.js';
 
 const router = express.Router();
 router.post('/', addSweet);
 
 router.delete('/:id', deleteSweet);
 router.get('/', getAllSweets);
+
+router.get('/search', searchAndSort);
 
 
 export default router;
